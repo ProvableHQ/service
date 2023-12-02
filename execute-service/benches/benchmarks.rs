d3573ad3025e252b5a210ec9578f2a7f878dbf8b
Pranav Gaddamadugu <23022326+d0cd@users.noreply.github.com>
2023-12-01 16:11:12 -0800

    Fix benchmark

--- execute-service/benches/benchmarks.rs
+++ execute-service/benches/benchmarks.rs
@@ -17,14 +17,18 @@
 #[macro_use]
 extern crate criterion;
 
-use criterion::{BatchSize, Criterion};
 use snarkvm::circuit::AleoV0;
 use snarkvm::prelude::{
-    Address, FromBytes, Identifier, Literal, PrivateKey, Process, ProgramID, Signature, Value, U64,
+    Address, Literal, PrivateKey, Process, Value, U64, ToBytes,
 };
-use std::str::FromStr;
 
 use execute_service::{CurrentNetwork, execute, ExecuteRequest};
+
+use criterion::{BatchSize, Criterion};
+use rand_chacha::rand_core::SeedableRng;
+use std::str::FromStr;
+use warp::hyper::body::Bytes;
+
 
 fn bench_execute_transfer_public(c: &mut Criterion) {
     let process = Process::<CurrentNetwork>::load().unwrap();
@@ -40,7 +44,7 @@ fn bench_execute_transfer_public(c: &mut Criterion) {
         )),
         Value::from(Literal::U64(U64::new(100))),
     ];
-    let rng = &mut rand::thread_rng();
+    let rng = &mut rand_chacha::ChaCha20Rng::from_entropy();
 
     let function_authorization = process
         .authorize::<AleoV0, _>(
@@ -53,9 +57,9 @@ fn bench_execute_transfer_public(c: &mut Criterion) {
         .unwrap();
 
     // Retrieve the execution ID.
-    let execution_id = execute_authorization.to_execution_id().unwrap();
+    let execution_id = function_authorization.to_execution_id().unwrap();
 
-    let fee_authorization = process.authorize_fee_public(
+    let fee_authorization = process.authorize_fee_public::<AleoV0, _>(
         &private_key,
         100000,
         1000,
@@ -64,15 +68,16 @@ fn bench_execute_transfer_public(c: &mut Criterion) {
     ).unwrap();
 
     let request = ExecuteRequest {
-        function_authorization: (),
-        fee_authorization: (),
+        function_authorization,
+        fee_authorization,
         state_root: None,
         state_path: None,
     };
+    let bytes = Bytes::copy_from_slice(&request.to_bytes_le().unwrap());
     c.bench_function("authorize_transfer_public", |b| {
         b.iter_batched(
-            || request.clone(),
-            |request| authorize(request).unwrap(),
+            || bytes.clone(),
+            |bytes| execute(bytes).unwrap(),
             BatchSize::SmallInput,
         )
     });
